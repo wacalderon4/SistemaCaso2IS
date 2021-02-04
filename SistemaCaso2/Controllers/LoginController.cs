8ed2ad0f1cda598b47d0dd223c34520e611e9f18
wacalderon4 <wacalderon4@utpl.edu.ec>
2021-02-04 01:08:29 -0500

    Login agregado

---
+++ SistemaCaso2/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaCaso2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Login(string User, string Pass)
+        {
+            try
+            {
+                
+                if(User == "wacalderon4" && Pass=="BpX8vLHk")
+                {
+                    return RedirectToAction("Principal", "Home");
+                }
+                else
+                {
+                    if (User == null && Pass == null)
+                    {
+                        ViewBag.Error = "Ingrese su usuario y contraseña";
+                        return View();
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Usuario y/o contraseña incorrectos";
+                        return View();
+                    }
+                }
+            }catch(Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpPost]
+
+        public ActionResult CerrarSesion()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaCaso2/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaCaso2.Models
+{
+    public partial class Login
+    {
+        public string User { get; set;}
+        public string Pass { get; set;}
+    }
+}
